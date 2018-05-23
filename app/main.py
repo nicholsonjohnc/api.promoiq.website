0fa350cc22ccb8eb5f2b955b7f4f8842b5b6e7c4
EC2 Default User <ec2-user@ip-172-31-75-109.ec2.internal>
2018-05-23 17:19:29 +0000

    Set host and port for production

--- app/main.py
+++ app/main.py
@@ -15,4 +15,5 @@ def generate_model():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=80)
+    # app.run(host='127.0.0.1', debug=True, port=8080)
