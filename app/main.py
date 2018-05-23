a68c77e47442ba8678fed647545aab88bfc1364d
EC2 Default User <ec2-user@ip-172-31-75-109.ec2.internal>
2018-05-23 13:00:03 +0000

    Modify return statement

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello():
-    return 'John Rules!'
+    return 'John Rules!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
